2af743b9bea0b5651af823413e6cc0368963cd8a
dragonborn612 <1078530492@qq.com>
2020-07-01 09:10:52 +0800

    2020年7月1日09:10:45

---
+++ Src/Client/Assets/LoadingSlider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSlider : MonoBehaviour
+{
+    public Slider loadingSlider;
+    public Text loadingText;
+    public  float loadTime=3f;
+    private float timer;
+    public GameObject loadingPanle;
+    public GameObject rigiestPanle;
+    public GameObject tipsPanle;
+	// Use this for initialization
+	void Start ()
+    {
+        loadingPanle.gameObject.SetActive(true);
+        rigiestPanle.gameObject.SetActive(false);
+        tipsPanle.gameObject.SetActive(true);
+        //loadingSlider = gameObject.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Loading();
+        Finishedloading();
+    }
+    void Loading()
+    {
+        timer += Time.deltaTime;
+        if (timer>=loadTime)
+        {
+            timer = loadTime;
+        }
+        double percentage = timer / loadTime;
+        loadingSlider.value =(float) percentage;
+        loadingText.text = "已加载" + percentage.ToString("P") ;
+    }
+    void Finishedloading()
+    {
+        if (loadingSlider.value==1)
+        {
+            loadingSlider.gameObject.SetActive(false);
+            rigiestPanle.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Src/Client/Assets/TipsAnimatonEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TipsAnimatonEvent : MonoBehaviour {
+
+	// Use this for initialization
+	public void TipsActive()
+    {
+        gameObject.SetActive (false);
+    }
+}
