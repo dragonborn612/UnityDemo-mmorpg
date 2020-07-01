9f926f96c521c2a3bf389f8311d378bd6b558f90
dragonborn612 <1078530492@qq.com>
2020-07-01 11:15:39 +0800

    2020年7月1日11:15:31

---
+++ Src/Client/Assets/Scripts/Services/UserService.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common;
+using Network;
+using UnityEngine;
+
+using SkillBridge.Message;
+
+namespace Services
+{
+    class UserService : Singleton<UserService>, IDisposable
+    {
+        public UnityEngine.Events.UnityAction<Result, string> OnRegister;
+        NetMessage pendingMessage = null;
+        bool connected = false;
+
+        public UserService()
+        {
+            NetClient.Instance.OnConnect += OnGameServerConnect;
+            NetClient.Instance.OnDisconnect += OnGameServerDisconnect;
+            MessageDistributer.Instance.Subscribe<UserRegisterResponse>(this.OnUserRegister);
+            
+        }
+
+        public void Dispose()
+        {
+            MessageDistributer.Instance.Unsubscribe<UserRegisterResponse>(this.OnUserRegister);
+            NetClient.Instance.OnConnect -= OnGameServerConnect;
+            NetClient.Instance.OnDisconnect -= OnGameServerDisconnect;
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void ConnectToServer()
+        {
+            Debug.Log("ConnectToServer() Start ");
+            //NetClient.Instance.CryptKey = this.SessionId;
+            NetClient.Instance.Init("127.0.0.1", 8000);
+            NetClient.Instance.Connect();
+        }
+
+
+        void OnGameServerConnect(int result, string reason)
+        {
+            Log.InfoFormat("LoadingMesager::OnGameServerConnect :{0} reason:{1}", result, reason);
+            if (NetClient.Instance.Connected)
+            {
+                this.connected = true;
+                if(this.pendingMessage!=null)
+                {
+                    NetClient.Instance.SendMessage(this.pendingMessage);
+                    this.pendingMessage = null;
+                }
+            }
+            else
+            {
+                if (!this.DisconnectNotify(result, reason))
+                {
+                    MessageBox.Show(string.Format("网络错误，无法连接到服务器！\n RESULT:{0} ERROR:{1}", result, reason), "错误", MessageBoxType.Error);
+                }
+            }
+        }
+
+        public void OnGameServerDisconnect(int result, string reason)
+        {
+            this.DisconnectNotify(result, reason);
+            return;
+        }
+
+        bool DisconnectNotify(int result,string reason)
+        {
+            if (this.pendingMessage != null)
+            {
+                if (this.pendingMessage.Request.userRegister!=null)
+                {
+                    if (this.OnRegister != null)
+                    {
+                        this.OnRegister(Result.Failed, string.Format("服务器断开！\n RESULT:{0} ERROR:{1}", result, reason));
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void SendRegister(string user, string psw)
+        {
+            Debug.LogFormat("UserRegisterRequest::user :{0} psw:{1}", user, psw);
+            NetMessage message = new NetMessage();
+            message.Request = new NetMessageRequest();
+            message.Request.userRegister = new UserRegisterRequest();
+            message.Request.userRegister.User = user;
+            message.Request.userRegister.Passward = psw;
+
+            if (this.connected && NetClient.Instance.Connected)
+            {
+                this.pendingMessage = null;
+                NetClient.Instance.SendMessage(message);
+            }
+            else
+            {
+                this.pendingMessage = message;
+                this.ConnectToServer();
+            }
+        }
+
+        void OnUserRegister(object sender, UserRegisterResponse response)
+        {
+            Debug.LogFormat("OnUserRegister:{0} [{1}]", response.Result, response.Errormsg);
+
+            if (this.OnRegister != null)
+            {
+                this.OnRegister(response.Result, response.Errormsg);
+            }
+        }
+    }
+}
